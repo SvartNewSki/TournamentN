d9e2e3d9857f3f4f334ec827a2bf73ef114dfb82
SvartNewSki <makhmudovAmir@gmail.com>
2025-07-02 17:43:15 +0300

    add routing

--- src/index.php
+++ src/index.php
@@ -6,7 +6,18 @@ require_once __DIR__ . "/vendor/autoload.php";
 require_once __DIR__ . "/config/app.php";
 
 
+
 $view = new BaseView();
 // $view->layout = 'tournaments';
 $view->render();
 
+
+$uri = $_SERVER['REQUEST_URI'];
+
+if ($uri === '/') {
+    require 'views/layouts/main.php';
+} elseif ($uri === '/about') {
+    require 'views/layouts/Tournaments.php';
+} else {
+    require 'pages/404.php';
+}
