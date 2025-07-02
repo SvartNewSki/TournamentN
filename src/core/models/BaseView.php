41d90f7e6af2f203ca3ad041bde4231b9e36442f
SvartNewSki <makhmudovAmir@gmail.com>
2025-07-02 17:45:04 +0300

    delete comment

--- src/core/models/BaseView.php
+++ src/core/models/BaseView.php
@@ -1,15 +1,7 @@
 <?php
 namespace core\models;
 
-// class BaseView
-// {
-//     public string $layout = 'main';
-//     public function render()
-//     {
-//         $fileLayout = LAYOUT_PATH . $this->layout . ".php";
-//         echo file_get_contents($fileLayout);
-//     }
-// }
+
 
 class BaseView
 {
