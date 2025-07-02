dccc872b050ceaf17206118992038287dcdd101c
SvartNewSki <makhmudovAmir@gmail.com>
2025-07-02 17:40:16 +0300

    add const PAGES_PATH

--- src/config/app.php
+++ src/config/app.php
@@ -3,3 +3,4 @@
 const VIEW_PATH = __DIR__ . "/../views/";
 const LAYOUT_PATH = VIEW_PATH . "layouts/";
 const BLOCKS_PATH = LAYOUT_PATH . "blocks/";
+const PAGES_PATH = __DIR__ . "/../views/layouts/";
