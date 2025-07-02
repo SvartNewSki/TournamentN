de0f76780e86b8f48f3c24005bb76a8c40f0d950
SvartNewSki <makhmudovAmir@gmail.com>
2025-07-02 12:58:02 +0300

    Rename index to main

---
+++ src/views/layouts/main.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="/css/style.css" />
+    <title>Document</title>
+  </head>
+  <body>
+    <div class="main">
+      <div class="title">WEBSITE NAME</div>
+      <div class="subtitle">Lorem Ipsum Text</div>
+      <div class="postmain">
+        <img src="/img/Mask group.svg" alt="" />
+        <div class="text">
+          <div class="text_block">
+            <div class="text_title">
+              Lorem Ipsum <br />
+              FGF Playoff
+            </div>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Duis
+            pulvinar. Donec ipsum massa, ullamcorper in, auctor et, scelerisque
+            sed, est. Nulla accumsan, elit sit amet varius semper, nulla mauris
+            mollis quam, tempor suscipit diam nulla vel leo. Vivamus luctus
+            egestas leo. Nullam sit amet magna in magna gravida vehicula. In
+            enim a arcu imperdiet malesuada. Sed elit dui, pellentesque a,
+            faucibus vel, interdum nec, diam. In rutrum. Sed ac dolor sit amet
+            purus malesuada congue
+          </div>
+          <div class="text_block">
+            <div class="text_title">
+              Lorem Ipsum <br />
+              FGF Playoff
+            </div>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Duis
+            pulvinar. Donec ipsum massa, ullamcorper in, auctor et, scelerisque
+            sed, est. Nulla accumsan, elit sit amet varius semper, nulla mauris
+            mollis quam, tempor suscipit diam nulla vel leo. Vivamus luctus
+            egestas leo. Nullam sit amet magna in magna gravida vehicula. In
+            enim a arcu imperdiet malesuada. Sed elit dui, pellentesque a,
+            faucibus vel, interdum nec, diam. In rutrum. Sed ac dolor sit amet
+            purus malesuada congue.
+          </div>
+        </div>
+      </div>
+      <div class="lower_block">
+        <div class="titleRG">RELEASED <span class="games"> GAMES</span></div>
+        <div class="text_lower_block">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Itaque earum
+          rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus
+          maiores alias consequatur aut perferendis doloribus asperiores
+          repellat.
+        </div>
+      </div>
+      <div class="tournament_announce">
+        <img src="/img/Mask group.png" alt="" />
+        <div class="tournament_right">
+          <div class="game_title_announce">CALL OF DUTY</div>
+          <div class="tournament_info">
+            <ul class="tournament_table">
+              <li>tournament info: August 8, 2028, 10pm, Los Angeles</li>
+              <li>Register: Until August 6, 2028, 8am</li>
+              <li>
+                Lorem ipsum dolor sit amet: Nulla turpis magna, cursus sit amet
+              </li>
+              <li>Lorem ipsum dolor: magna, cursus sit amet</li>
+              <li>sit amet: cursus sit amet</li>
+            </ul>
+          </div>
+          <img class="button" src="/img/Vector 3.png" alt="" />
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
