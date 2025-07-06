f5b0e1ac0194f8e98a4466b55b31af07ce914d76
SvartNewSki <makhmudovAmir@gmail.com>
2025-07-06 13:24:53 +0300

    fix app.php const PATH

--- src/config/app.php
+++ src/config/app.php
@@ -2,5 +2,5 @@
 
 const VIEW_PATH = __DIR__ . "/../views/";
 const LAYOUT_PATH = VIEW_PATH . "layouts/";
-const BLOCKS_PATH = LAYOUT_PATH . "blocks/";
+const COMPONENTS_PATH = LAYOUT_PATH . "components/";
 const PAGES_PATH = __DIR__ . "/../views/layouts/";
