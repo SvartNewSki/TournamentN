e76a44c927084f26cdd63c98ed61a2f73f3ac74d
SvartNewSki <makhmudovAmir@gmail.com>
2025-07-07 11:38:29 +0300

    add correct routes for pages in header

--- src/views/layouts/components/header.php
+++ src/views/layouts/components/header.php
@@ -1,23 +1,25 @@
 <div class="header">
-    <div class="logo">LOGO</div>
+    <div class="logo">
+        <img src="/img/logo-fox-flatten.svg" alt="FB" height="100 em" />
+    </div>
     <div class="nav">
     <ul class="ul_nav">
-        <li><a href="/index.php">Home</a></li>
-        <li><a href="/Tournaments.php">Tournament</a></li>
+        <li><a href="/index">Home</a></li>
+        <li><a href="/Tournaments">Tournaments</a></li>
         <?php
             if(isset($_COOKIE['login'])) { ?>
-                <li><a href="PAGES_PATH">Кабинет пользователя</a></li>
+                <li><a href="/user">Кабинет пользователя</a></li>
                 <li><a href="/lib/exit.php">Выйти</a></li>
                 <?php }
             else{ ?>
-               <li><a href="/auth.php">Auth</a></li>
-               <li><a href="/register.php">Register</a></li>
+               <li><a href="/auth">Auth</a></li>
+               <li><a href="/register">Register</a></li>
            <?php }
         ?>
 
-        <li>News</li>
+        <li><a href="/news">News</a></li>
         
     </ul>
     </div>
-    <div class="lang">EN</div>
+    <div class="lang">RU</div>
 </div>
