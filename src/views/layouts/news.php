95189c38fadc8fd0e696b4f7fa3bd18b279603ba
SvartNewSki <makhmudovAmir@gmail.com>
2025-07-07 11:37:43 +0300

    add news page

--- src/views/layouts/news.php
+++ src/views/layouts/news.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    News
-</body>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="/css/style.css" />
+    <link rel="stylesheet" href="/css/tournaments.css">
+    <title>News</title>
+  </head>
+  <body>
+  
+    <div class="title_text"> 
+        <p><span class="text_color"> NEWS </span></p>
+    </div>
+    </div>
+    </div>
+  </body>
 </html>
