b29d8237b820abff96056c4bf353490e2afcbf59
SvartNewSki <makhmudovAmir@gmail.com>
2025-07-08 21:44:46 +0300

    delete commentaries

--- src/index.php
+++ src/index.php
@@ -8,16 +8,6 @@ use core\Router\Router;
 require_once __DIR__ . "/vendor/autoload.php";
 require_once __DIR__ . "/config/app.php";
 
-// require_once __DIR__ . "/routes/web.php";
-
-// $view = new BaseView();
-
-// $view->render();
-
-// $uri = $_SERVER['REQUEST_URI'];
-
-// App::run();
-
 $router = new Router();
 require_once __DIR__ . '/routes/web.php';
 $router->dispatch($_SERVER["REQUEST_METHOD"],$_SERVER["REQUEST_URI"]);
