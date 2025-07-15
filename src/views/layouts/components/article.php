f79583713bc7c54eccd9b6e430b675adbc2e4531
SvartNewSki <makhmudovAmir@gmail.com>
2025-07-15 22:59:49 +0300

    Add components

---
+++ src/views/layouts/components/article.php
@@ -0,0 +1,5 @@
+<a class="card" href="/news/<?= $id?>" >
+    <h2><?=$article['title']?></h2>
+    <p><?=substr($article['description'], 0, 250)?></p>
+    <img src="/assets/img/Frame.jpg" alt="">
+</a> 
---
+++ src/views/layouts/components/tournament-slide.php
@@ -0,0 +1,4 @@
+<a href="/tournament/<?= $id ?>" class="slide">
+    <img  src="<?= $image  ?>" alt="" >
+    <h2><?= $tournament['title'] ?></h2>
+</a>
