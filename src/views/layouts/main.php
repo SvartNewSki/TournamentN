ccfab6f47743b95c5bdad53925bcac8fe09c72ac
SvartNewSki <makhmudovAmir@gmail.com>
2025-07-15 23:04:20 +0300

    Add news and Tournaments to main page

--- src/views/layouts/main.php
+++ src/views/layouts/main.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="/assets/css/style.css" />
+    <link rel="stylesheet" href="/assets/css/card.css" />
         
     <title>Tournament</title>
   </head>
@@ -14,18 +15,25 @@
         <div class="titleInfo">
           <div class="postmain">
             <div class="carousel-images">
-              <img  src="/img/witcher.jpg" alt="" >
-              <img  src="/img/sus.jpg" alt="">
-              <img  src="/img/cs2.png" alt="">
-            
+              <?php
+                use core\models\BaseView;
+                $i = 0;
+                foreach($tournaments as $key => $tournament) {
+                  BaseView::renderSingle('/components/tournament-slide', array_merge([
+                    'image' => $images[$i] ?? "/img/witcher.jpg",
+                    'tournament' => $tournament,
+                    'id' => $key,
+                  ]));
+                  $i++;
+                }
+              ?>
             </div>
-              <button class="carousel-btn prev-btn">&lt;</button>
-              <button class="carousel-btn next-btn">&gt;</button>
-            
+            <button class="carousel-btn prev-btn">&lt;</button>
+            <button class="carousel-btn next-btn">&gt;</button>
           </div>
       </div>
       <div class="lower_block">
-        <div class="titleRG">RELEASED <span class="games"> GAMES</span></div>
+        <div class="titleRG">LAST <span class="games">NEWS</span></div>
         <div class="text_lower_block">
           Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Itaque earum
           rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus
@@ -33,7 +41,16 @@
           repellat.
         </div>
       </div>
-     
+      <div class="news cards">
+        <?php
+          foreach($news as $key => $article){
+            BaseView::renderSingle('/components/article', [
+              'article' => $article,
+              'id' => $key
+            ]);
+          }
+        ?>
+      </div>
     </div>
   <script src="/js/script.js"></script>
   </body>
